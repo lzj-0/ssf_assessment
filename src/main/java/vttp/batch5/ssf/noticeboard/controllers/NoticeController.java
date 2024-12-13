d1d615dc2e3349a93e027b7365a6845f82f67245
lzj-0 <jay260399@gmail.com>
2024-12-13 09:04:05 +0800

    added template

---
+++ src/main/java/vttp/batch5/ssf/noticeboard/controllers/NoticeController.java
@@ -0,0 +1,9 @@
+package vttp.batch5.ssf.noticeboard.controllers;
+
+import org.springframework.stereotype.Controller;
+
+// Use this class to write your request handlers
+@Controller
+public class NoticeController {
+
+}
---
+++ src/main/java/vttp/batch5/ssf/noticeboard/services/NoticeService.java
@@ -0,0 +1,14 @@
+package vttp.batch5.ssf.noticeboard.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService {
+
+	// TODO: Task 3
+	// You can change the signature of this method by adding any number of parameters
+	// and return any type
+	public void postToNoticeServer() {
+
+	}
+}
